a9220e249d8136f04d48c65f9dc302b67e3dace9
rinald <rinald.shehaj@albash.al>
2021-07-03 10:17:30 +0200

    Fixings 2

--- resources/views/calculator/index.blade.php
+++ resources/views/calculator/index.blade.php
@@ -2,39 +2,56 @@
 
 @section('content')
     <div class="pb-4">
-        <form name="calculator_form" id="calculator_form" method="POST" action="{{ route('calculate') }}" enctype="multipart/form-data">
+        <form class="container" name="calculator_form" id="calculator_form" method="POST" action="{{ route('calculate') }}" enctype="multipart/form-data">
         @csrf
-        <label>From</label>
-        <select name="fromCountry" id="fromCountry">
-            @foreach($countries as $country)
-                <option value="{{ $country->name  }}"> {{ $country->name }}</option>
-            @endforeach
-        </select>
-        <label>To</label>
-        <select name="toCountry" id="toCountry">
-            @include('partials.countries_select_options')
-        </select>
+            <div class="d-flex">
+                <div class="d-flex justify-content-around">
+                    <div class="d-flex flex-column">
+                        <label class="form-label">From</label>
+                        <select class="form-control" name="fromCountry" id="fromCountry">
+                            @foreach($countries as $country)
+                                <option value="{{ $country->name  }}"> {{ $country->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <label class="form-label">To</label>
+                        <select class="form-control" name="toCountry" id="toCountry">
+                            @include('partials.countries_select_options')
+                        </select>
+                    </div>
+                </div>
+            </div>
+                <div class="d-flex justify-content-around">
+                    <div class="d-flex flex-column">
+                        <label class="form-label">Length</label>
+                        <input class="form-control" type="text" name="length">
+                    </div>
+                    <div class="d-flex flex-column">
+                        <label class="form-label">Height</label>
+                        <input class="form-control" type="text" name="height">
+                    </div>
+                </div>
+            </div>
         <div>
-            <label>Length</label>
-            <input type="text" name="length">
-            <label>Height</label>
-            <input type="text" name="height">
-            <label>Width</label>
-            <input type="text" name="width">
-            <label>Weight</label>
-            <input type="text" name="weight">
+            <label class="form-label">Width</label>
+            <input class="form-control" type="text" name="width">
+            <label class="form-label">Weight</label>
+            <input class="form-control" type="text" name="weight">
         </div>
         <input type="submit" value="Calculate" class="btn btn-primary calculate__js">
     </form>
     </div>
-    <table class="table d-none" name="rates_table" id="rates_table" >
-        <thead>
+    <div class="container">
+        <table class="table d-none" name="rates_table" id="rates_table">
+            <thead>
             <th>Carrier</th>
             <th>Service</th>
             <th>Rate</th>
-        </thead>
-        <tbody>
+            </thead>
+            <tbody>
 
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,89 +1,171 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{asset('img/favicon.ico')}}"/>
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900"
+          rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i"
+          rel="stylesheet"/>
+    <link href="{{asset('css/styles.css')}}" rel="stylesheet"/>
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                    <a href="{{ route('calculator') }}">Calculator</a>
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body id="#">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container px-5">
+        <a class="navbar-brand" href="#">Forward Delivery</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
+                class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Sign Up</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Log In</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Header-->
+<header class="masthead text-center text-white">
+    <div class="masthead-content">
+        <div class="container px-5">
+            <h1 class="masthead-heading mb-0">All in one shipping solution</h1>
+            <h2 class="masthead-subheading mb-0">The best way to ship products globally to your location.</h2>
+            <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a>
+        </div>
+    </div>
+</header>
+<!-- Content section 1-->
+<section id="scroll">
+    <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-lg-6 order-lg-2">
+                <div class="p-5"><img class="img-fluid rounded-circle" src="{{asset('img/address.jpg')}}" alt="..."/>
                 </div>
-            @endif
-            <div class="content">
-                <div class="title m-b-md">
-                    Welcome
+            </div>
+            <div class="col-lg-6 order-lg-1">
+                <div class="p-5">
+                    <h2 class="display-4">Create your address</h2>
+                    <p>You can create your address for any country that we provide. </p>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</section>
+<!-- Content section 2-->
+<section>
+    <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-lg-6">
+                <div class="p-5"><img class="img-fluid rounded-circle" src="{{asset('img/shop_and_ship.jpeg')}}"
+                                      alt="..."/>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="p-5">
+                    <h2 class="display-4">Shop and ship</h2>
+                    <p>You can shop and ship from anywhere to anywhere. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Content section 3-->
+<section>
+    <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-lg-6 order-lg-2">
+                <div class="p-5"><img class="img-fluid rounded-circle" src="{{asset('img/shipping-calculator.png')}}"
+                                      alt="..."/>
+                </div>
+            </div>
+            <div class="col-lg-6 order-lg-1">
+                <div class="p-5">
+                    <h2 class="display-4">Calculate your oreder.</h2>
+                    <p>You can calculate the price of your order based on the prices from the carriers, dimensions
+                        etc.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Footer-->
+<footer class="py-5 text-light bg-dark">
+    <div class="container px-5 d-flex justify-content-between">
+        <div>
+            <p>Address info details</p>
+        </div>
+        <div>
+            <p>Address info details</p>
+        </div>
+        <div>
+            <p>Address info details</p>
+        </div>
+    </div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
