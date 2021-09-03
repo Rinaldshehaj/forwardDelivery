766c128824cb7d30685075909c7aaf37a51519da
rinald <rinald.shehaj@albash.al>
2021-09-03 21:57:33 +0200

    Calculator design improved

--- resources/views/calculator/index.blade.php
+++ resources/views/calculator/index.blade.php
@@ -2,49 +2,63 @@
 
 @section('content')
     <div class="pb-4">
-        <form class="container d-flex justify-content-around flex-wrap" name="calculator_form" id="calculator_form"
+        <div class="container-fluid">
+            <div class="container d-flex justify-content-center">
+                <h2 class="mb-5 mt-4">This will be a desc</h2>
+            </div>
+        </div>
+        <form class="container rounded shadow-lg" name="calculator_form" id="calculator_form"
               method="POST"
               action="{{ route('calculate') }}" enctype="multipart/form-data">
             @csrf
-            <div class="d-flex align-self-center">
-                <div class="d-flex flex-column">
-                    <label class="form-label">From</label>
-                    <select class="form-control" name="fromCountry" id="fromCountry">
-                        @foreach($countries as $country)
-                            <option value="{{ $country->name  }}"> {{ $country->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="d-flex flex-column">
-                    <label class="form-label">To</label>
-                    <select class="form-control" name="toCountry" id="toCountry">
-                        @include('partials.countries_select_options')
-                    </select>
-                </div>
+            <div class="p-4">
+                <h3>The calculator</h3>
             </div>
-            <div class="d-flex align-self-center flex-column">
-                <div class="d-flex justify-content-around">
+            <hr/>
+            <div class="d-flex justify-content-center">
+                <h2>Sponsors</h2>
+            </div>
+            <div class="d-flex justify-content-between flex-wrap p-5">
+                <div class="d-flex flex-column">
                     <div class="d-flex flex-column">
-                        <label class="form-label">Length</label>
-                        <input class="form-control" type="text" name="length">
+                        <label class="form-label">From</label>
+                        <select class="form-control" name="fromCountry" id="fromCountry">
+                            @foreach($countries as $country)
+                                <option value="{{ $country->name  }}"> {{ $country->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
                     <div class="d-flex flex-column">
-                        <label class="form-label">Height</label>
-                        <input class="form-control" type="text" name="height">
+                        <label class="form-label">To</label>
+                        <select class="form-control" name="toCountry" id="toCountry">
+                            @include('partials.countries_select_options')
+                        </select>
                     </div>
                 </div>
-                <div class="d-flex justify-content-around">
-                    <div class="d-flex flex-column">
-                        <label class="form-label">Width</label>
-                        <input class="form-control" type="text" name="width">
+                <div class="d-flex align-self-center flex-column">
+                    <div class="d-flex justify-content-around">
+                        <div class="d-flex flex-column mr-5">
+                            <label class="form-label">Length</label>
+                            <input class="form-control" type="text" name="length">
+                        </div>
+                        <div class="d-flex flex-column">
+                            <label class="form-label">Height</label>
+                            <input class="form-control" type="text" name="height">
+                        </div>
                     </div>
-                    <div class="d-flex flex-column">
-                        <label class="form-label">Weight</label>
-                        <input class="form-control" type="text" name="weight">
+                    <div class="d-flex justify-content-around">
+                        <div class="d-flex flex-column mr-5">
+                            <label class="form-label">Width</label>
+                            <input class="form-control" type="text" name="width">
+                        </div>
+                        <div class="d-flex flex-column">
+                            <label class="form-label">Weight</label>
+                            <input class="form-control" type="text" name="weight">
+                        </div>
                     </div>
-                </div>
-                <div class="pt-5">
-                    <input type="submit" value="Calculate" class="btn btn-primary calculate__js">
+                    <div class="pt-5">
+                        <input type="submit" value="Calculate" class="btn btn-primary calculate__js">
+                    </div>
                 </div>
             </div>
         </form>
